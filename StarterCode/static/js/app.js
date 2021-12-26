628136cd3ca02f364b71737cfb553ffddee6133d
Andunguh <anthonynndungu@gmail.com>
2021-12-26 15:01:48 -0500

    update

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,19 @@
+
+
+
+// function that updates dashboard
+
+//function that populates meta data
+
+//function that builds graphs
+
+// function that initializes dashboard
+function initialize()
+{
+    //load data from j.son file
+    let data = d3.json("samples.json");
+    console.log(data);
+}
+
+// call the intialize function
+initialize();
