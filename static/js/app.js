87aec1771f71f75bcbfb7af425c53688cf610603
Andunguh <anthonynndungu@gmail.com>
2022-01-01 01:34:08 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -10,10 +10,18 @@
 // function that initializes dashboard
 function initialize()
 {
-    //load data from j.son file
+     // load data from j.son file
     let data = d3.json("samples.json");
     console.log(data);
-}
+ 
+     // access the dropdown selector from the index.html file
+    var select = d3.select("#selDataset");
+ 
+     // use d3.json in order to get the data
+ 
+    let sampleNames =
+}        
+
 
 // call the intialize function
 initialize();
